8f076a454689e4ebe53483a0c4fdcfa00b8f0510
Cezar Lemos <cezarcbl@protonmail.com>
2020-02-03 17:59:08 -0300

    .

--- command.py
+++ command.py
@@ -1,26 +1,10 @@
-"""Helper file to call scripts."""
+"""Helper file to call scripts and register inputs on code."""
 import subprocess
 from traindet import config as cfg
 
-dataset = 'mixed'
-epochs = '100'
 
-
-command = f"""
-    python scripts/train_ssd.py 
-        --transfer
-        --base-model ssd_512_resnet50_v1_coco
-        --data-shape 512
-        --dataset {dataset}
-        --save-prefix {cfg.checkpoints_folder}/{dataset}/ssd512_mixed_time_benchmark/
-        --batch-size 4
-        --epochs {epochs}
-        --lr 0.001
-        --lr-decay 0.1
-        --lr-decay-epoch 60,80
-        --seed 233
-"""
-
+# dataset = 'mixed'
+# epochs = '100'
 
 
 # command = f"""
@@ -29,16 +13,15 @@ command = f"""
 #         --base-model ssd_512_resnet50_v1_coco
 #         --data-shape 512
 #         --dataset {dataset}
-#         --save-prefix {cfg.checkpoints_folder}/{dataset}/ssd512_mixed_bilinear_grayscale/
+#         --save-prefix {cfg.checkpoints_folder}/{dataset}/ssd512_mixed_time_benchmark/
 #         --batch-size 4
 #         --epochs {epochs}
 #         --lr 0.001
 #         --lr-decay 0.1
 #         --lr-decay-epoch 60,80
 #         --seed 233
-#         --bilateral-kernel-size 9
-#         --sigma-vals 75
-#         --grayscale
 # """
 
-subprocess.call(command.split())
+
+if __name__ == '__main__':
+    subprocess.call(command.split())
