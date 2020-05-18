26aa8e8aaf4a1b2da58fb8925e0d18dab0c97bfb
Cézar Lemos <cezarcbl@protonmail.com>
2020-05-18 02:32:26 -0300

    fix script

--- docker/post_install.sh
+++ docker/post_install.sh
@@ -5,9 +5,10 @@ bash ~/miniconda.sh -p $HOME/miniconda
 source ~/.bashrc
 # source ~/miniconda/etc/profile.d/conda.sh
 conda create -n traindet python=3.7
+eval "$(conda shell.bash hook)"
 conda activate traindet
 # source /home/$USER/.bashrc
-cd /home/$USER/Projects
+cd /home/$USER
 git clone https://github.com/czrcbl/bboxes
 cd bboxes
 pip install -e .
