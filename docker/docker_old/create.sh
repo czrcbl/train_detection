e6a370d267d996ab8fe015dfa37ddb2694e150a3
Cézar Lemos <cezarcbl@protonmail.com>
2020-06-22 19:31:54 -0300

    update docker

--- docker/docker_old/create.sh
+++ docker/docker_old/create.sh
@@ -1,13 +1,7 @@
 #!/usr/bin/bash
+$containerName=cuda101man
 docker run -it \
-    --mount type=bind,source=/home/$USER/traindet_docker,target=/home/user \
+    --mount type=bind,source=/home/$USER,target=/home/$USER \
     --gpus all \
-    --env="DISPLAY" \
-    --env="QT_X11_NO_MITSHM=1" \
-    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
-    --name traindet \
-    traindet
-
-export containerId=$(docker ps -l -q)
-xhost +local:'docker inspect --format='' $containerId'
-docker start -a -i $containerId
+    --name cuda101man \
+    czrcbl/dev:cuda101man
