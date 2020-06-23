a14f63f5a801e6092ee0940209c4795081f122b9
Cezar Lemos <cezarcbl@protonmail.com>
2020-06-23 19:31:43 -0300

    fix data

--- scripts/train_frcnn.py
+++ scripts/train_frcnn.py
@@ -4,13 +4,13 @@ import argparse
 import os
 
 # disable autotune
-os.environ['MXNET_CUDNN_AUTOTUNE_DEFAULT'] = '0'
-os.environ['MXNET_GPU_MEM_POOL_TYPE'] = 'Round'
-os.environ['MXNET_GPU_MEM_POOL_ROUND_LINEAR_CUTOFF'] = '26'
-os.environ['MXNET_EXEC_BULK_EXEC_MAX_NODE_TRAIN_FWD'] = '999'
-os.environ['MXNET_EXEC_BULK_EXEC_MAX_NODE_TRAIN_BWD'] = '25'
-os.environ['MXNET_GPU_COPY_NTHREADS'] = '1'
-os.environ['MXNET_OPTIMIZER_AGGREGATION_SIZE'] = '54'
+# os.environ['MXNET_CUDNN_AUTOTUNE_DEFAULT'] = '0'
+# os.environ['MXNET_GPU_MEM_POOL_TYPE'] = 'Round'
+# os.environ['MXNET_GPU_MEM_POOL_ROUND_LINEAR_CUTOFF'] = '26'
+# os.environ['MXNET_EXEC_BULK_EXEC_MAX_NODE_TRAIN_FWD'] = '999'
+# os.environ['MXNET_EXEC_BULK_EXEC_MAX_NODE_TRAIN_BWD'] = '25'
+# os.environ['MXNET_GPU_COPY_NTHREADS'] = '1'
+# os.environ['MXNET_OPTIMIZER_AGGREGATION_SIZE'] = '54'
 
 import logging
 import time
